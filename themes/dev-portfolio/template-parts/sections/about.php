59cac60df69f1512c981b2187d77ec993fb5ed13
Marcin Potoczny <marcin.potoczny@protonmail.com>
2026-07-13 18:39:30 +0200

    Add developer image to about section

--- themes/dev-portfolio/template-parts/sections/about.php
+++ themes/dev-portfolio/template-parts/sections/about.php
@@ -2,25 +2,40 @@
 
     <div class="container">
 
-
-        <div class="about__content">
-
-
-            
+        <div class="about__grid">
 
 
-            <h2 class="about__title">
-                <?php echo esc_html(get_field('about_title')); ?>
-            </h2>
+            <div class="about__content">
 
 
-            <p class="about__description">
-                <?php echo esc_html(get_field('about_description')); ?>
-            </p>
+                <h2 class="about__title">
+                    <?php echo esc_html(get_field('about_title')); ?>
+                </h2>
+
+
+                <p class="about__description">
+                    <?php echo esc_html(get_field('about_description')); ?>
+                </p>
+
+
+            </div>
+
+
+
+            <div class="about__logo">
+
+
+                <img 
+                    class="about__logo-image"
+                    src="<?php echo get_template_directory_uri(); ?>/assets/images/developer-avatar.png"
+                    alt="Software Developer"
+                >
+
+
+            </div>
 
 
         </div>
-
 
     </div>
 
